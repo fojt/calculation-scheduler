de564e4d98d7caf0ef4bb90e7632d78b946adfcb
vojtechkalab <kalab@vojtechkalab.eu>
2021-03-24 08:01:06 +0100

    one hour test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'calculation-scheduler';
+  public types = ['basic', 'full', 'fast'];
+  public name = new FormControl('');
+  public type = new FormControl(this.types[0]);
+  public date = new FormControl('');
+  public repeats = new FormControl('');
+  public produceReport = new FormControl('');
+
+  public schedulerList = [];
+
+  constructor(public httpClient: HttpClient) {
+  }
+
+  public ngOnInit() {
+    /* call load data */
+  }
+
+  /* this should be in a service */
+  private loadSchedulerList(): void {
+    this.httpClient
+      .request('Get', 'testUrl/getList', {})
+      .subscribe(
+        (response) => {
+          // this.schedulerList = response.data
+        });
+  }
+
+  /* this should be in a service */
+  public onSubmit(): void {
+    this.httpClient
+      .request('Post', 'testUrl/Post', {})
+      .subscribe(
+        (response) => {
+        },
+        error => {
+
+        }
+      );
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule, MatOptionModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatOptionModule,
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
